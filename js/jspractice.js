de90e04f87b3d037f848c325f9ad106c5f341ff3
mizukikitagawa <mizuki.kitagawa@force-corp.co.jp>
2020-04-23 17:14:43 +0900

    java

---
+++ js/jspractice.js
@@ -0,0 +1,43 @@
+// document.write("Hellow World!");
+// alert("Hellow World!");
+
+//console.log("Hello World!" + "田中さん");
+//console.log(10 + 10);
+//console.log("10" + "10")
+
+//console.log(1 + 1);
+//console.log(1 - 1);
+//console.log(2 * 2);
+//console.log(10 / 3);
+//console.log(10 % 3);
+//console.log(10 % 3);
+
+// let x = 1;
+// let y = 1;
+//x++; //x = x + 1
+// console.log(x);
+// y--; //y = y-1;
+// console.log(y);
+
+//let x = 7;
+//let y = x++;
+//let x2 = 7;
+//let y2 = ++x2;
+
+//console.log(x);
+//console.log(y);
+//console.log(x2);
+//console.log(y2);
+
+//変数とは　値を代入するための箱
+//定数とは　値を別名で定義したもの
+
+`use strict`;
+
+let age = 30;
+const name = "田中太郎";
+
+age = age + 2;
+
+console.log(`${name}さんの年齢は${age}です`);
+
